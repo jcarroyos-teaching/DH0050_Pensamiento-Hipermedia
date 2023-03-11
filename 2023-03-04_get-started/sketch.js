622434a321ed3485f8bea343cae2880f1e3da133
Juan Carlos <jcarroyos@users.noreply.github.com>
2023-03-11 10:30:29 -0500

    Update sketch.js
    Subo el codigo processing con el tutorial de POO

--- 2023-03-04_get-started/sketch.js
+++ 2023-03-04_get-started/sketch.js
@@ -1,12 +1,59 @@
+// Where is the car
+let x, y;
+
+// crear la clase Carro
+class Carro {
+  constructor(alto,ancho,color,name){
+    this.alto = alto;
+    this.ancho = ancho;
+    this.color = color;
+    this.name = name;
+  } 
+}
+
+
+
 function setup() {
-  createCanvas(400, 400);
+
+  createCanvas(400, 200);
+  // Start position
+  y = height - height/4;
+  x = 0;
+  // crear una instancia de Carro llamada miCarro
+  let miCarro = new Carro (10,20,(red),"miNave");
+  
+  console.log("miCarro ancho is: " + miCarro.ancho);
+  console.log("miCarro alto is: " + miCarro.alto);
+  console.log("Nombre de miCarro es: " + miCarro.name);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
+  background(200);  
+  display();
+  move();
+  //console.log(miCarro.alto);
+  
+  let miCarro = new Carro (10,20,(red),"miNave");
+  
+}
+
+function move(){
+  // Jiggling randomly
+  y = y + random(-1, 1);
+  
+  if (x > width) {
+    // Reset 
+    x = 0;
+    y = height - height/4;
   } else {
-    fill(255);
+    // Moving up at a constant speed
+    x = x + 1;
   }
-  ellipse(mouseX, mouseY, 80, 80);
+}
+
+function display(){
+  // Draw a car
+  stroke(50);
+  fill(255,4,150);
+  rect(x, y, 20, 10);
 }
